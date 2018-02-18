9e8c735fca60fedb53d18ab9317a626772bb854b
Guy Turner <guyturner797@users.noreply.github.com>
2018-02-18 17:28:54 +0000

    started creation of snake
    - built snake generator
    - added score value
    - added random food generator
    - added snake length array

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, time
+import pygame, time, random
 
 #initialise pygame
 pygame.init()
@@ -6,11 +6,15 @@ pygame.font.init()
 
 #define fonts
 font = pygame.font.SysFont('Arial', 75)
+smallfont = pygame.font.SysFont('Arial', 30)
 largefont = pygame.font.SysFont('Arial', 115)
 
 #display size
 display_width = 600
 display_height = 800
+
+#square size of snake and food (x&y)
+squaresize = 20
 
 #colours
 white = (255,255,255)
@@ -24,7 +28,6 @@ pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
 
 
-
 def text_objects(text,font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
@@ -36,13 +39,10 @@ def button(x, y, xi, yi, text, font, colour):
     pygame.draw.rect(gameDisplay, colour, [x, y,xi, yi])
     gameDisplay.blit(btnSurf, btnRect)
 
-
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
     if (x+xi) > mouse[0] > x and (y+yi) > mouse[1] > y:
-        print('dfg')
-        print(click)
         if click[0] == 1:
             return(1)
 
@@ -76,9 +76,38 @@ def game_intro():
 
 def play_game():
     gameDisplay.fill(white)
-    pygame.draw.rect(gameDisplay, black, [0, 0, display_width-1, display_height-1], 2)
+    pygame.draw.rect(gameDisplay, black, [0, 0, display_width, display_height], 1)
 
+    xposes = display_width/squaresize
+    yposes = display_height/squaresize
+    centrepos = [int(xposes/2), (yposes/2)]
+    print(centrepos)
+
+    #variables
     gameExit = False
+    length = 4
+    global food_location
+    food_location = [0,0]
+    snakesquares = []
+    score = 344
+
+    for num in range(length):
+        snakesquares.append([centrepos[0], centrepos[0]-num])
+
+    def new_food(snakesquares):
+        x = random.randint(0, xposes-1)
+        y = random.randint(0, yposes-1)
+
+        for snakesquare in snakesquares:
+            if x == snakesquare[0] and y ==snakesquare[1]:
+                new_food(snakesquares)
+        
+        pygame.draw.rect(gameDisplay, red, [x*20, y*20, 20, 20])
+        food_location = [x,y]
+        print(food_location)
+
+    new_food(snakesquares)
+
 
     while not gameExit:
         for event in pygame.event.get():
@@ -86,9 +115,17 @@ def play_game():
                 pygame.quit()
                 quit()
         
+        for snakesquare in snakesquares:
+            pygame.draw.rect(gameDisplay, green, [snakesquare[0]*20, snakesquare[1]*20,20, 20])
+
+
+
+        scoreSurf, scoreRect = text_objects(str(score), smallfont)
+        scoreRect.bottomright = (display_width-10,display_height-5)
+        gameDisplay.blit(scoreSurf, scoreRect)
             
         pygame.display.update()
-        clock.tick(5)
+        clock.tick(1)
     
 
 game_intro()
